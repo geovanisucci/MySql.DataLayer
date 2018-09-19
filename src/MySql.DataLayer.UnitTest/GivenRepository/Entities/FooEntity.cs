606c67dc243dd33be9dd4802c3a4dd2e22d164a4
sajoratosg <geovani.succi@sage.com>
2018-09-19 19:01:05 -0300

    TDD added

---
+++ src/MySql.DataLayer.UnitTest/GivenRepository/Entities/FooEntity.cs
@@ -0,0 +1,18 @@
+﻿using MySql.DataLayer.Attributes.EntityConfig.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySql.DataLayer.UnitTest.GivenRepository.Entities
+{
+    [TableName("foo")]
+    public class FooEntity : IDataEntity
+    {
+        [PK("id")]
+        public Guid id { get; set; }
+        [ColumnName("Description")]
+        public string Description { get; set; }
+    }
+}
---
+++ src/MySql.DataLayer.UnitTest/GivenRepository/When_CreateAsync_Is_Called/And_Create_Happens.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySql.DataLayer.UnitTest.GivenRepository.When_CreateAsync_Is_Called
+{
+    [TestClass]
+    public class And_Create_Happens
+    {
+        [TestMethod]
+        public void Should_Return_Success()
+        {
+            int result = 0;
+
+            string sqlCommand = "SELECT IFNULL((SELECT 1 FROM information_schema.tables WHERE table_schema = 'mysqlcoretest' AND table_name = 'foo' LIMIT 1),0) as TableExist;";
+
+            //Execute SQL Query
+            //using (var c = _connectionFactory.GetAsync().Result)
+            //{
+            //    result = c.ExecuteScalarAsync<int>(sqlCommand.ToString()).Result;
+            //}
+
+            Assert.IsTrue(result == 1);
+        }
+    }
+}
